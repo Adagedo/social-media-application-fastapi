d25c7937498a4fddd10eedece73f03a91afea882
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-31 22:35:20 +0100

    imported neccessary package and created a role class using enums and the followers tables

---
+++ api/Social/models/user.py
@@ -0,0 +1,28 @@
+from typing import List, Optional
+from datetime import datetime
+from enum import Enum
+from sqlalchemy import (
+    Column,
+    DateTime,
+    ForeignKey,
+    String,
+    Table,
+    func,
+    Enum as SQLAlchemyEnum,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from api.Social.models.abstractbase import AbstractBaseModel
+from api.Social.utils.database import Base
+
+
+class RoleEnum(Enum):
+    user = "user"
+    admin = "admin"
+
+
+followers_table = Table(
+    "user_interaction",
+    Base.metadata,
+    Column("follower_id", String, ForeignKey("user.id"), primary_key=True),
+    Column("followed_id", String, ForeignKey("user.id"), primary_key=True),
+)
