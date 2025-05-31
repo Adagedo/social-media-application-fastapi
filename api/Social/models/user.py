132e64f22c7811e06c0e29ceb34b559ed3b6824e
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-31 22:41:17 +0100

    implemented the table content and their relationships

--- api/Social/models/user.py
+++ api/Social/models/user.py
@@ -32,3 +32,20 @@ class User (AbstractBaseModel):
     
     __tablename__ = "user"
     
+    username: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(1024), nullable=False)
+    bio: Mapped[Optional[str]] = mapped_column(String(1024))
+    contact_info: Mapped[Optional[str]] = mapped_column(String(15))
+    followings = relationship(
+        "User",
+        secondary=followers_table,
+        primaryjoin=lambda: User.id == followers_table.c.followed_id,
+        secondaryjoin=lambda: User.id == followers_table.c.follower_id,
+        backref="followers",
+    )
+    role: Mapped[str] = mapped_column(SQLAlchemyEnum(RoleEnum), default=RoleEnum.user)
+    last_login: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True))
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
