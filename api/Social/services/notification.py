d644cbd763e18f70a85203416622850a304e7720
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-02 11:21:27 +0100

    created the event_generator method

---
+++ api/Social/services/notification.py
@@ -0,0 +1,27 @@
+from fastapi import BackgroundTasks
+from sqlalchemy.orm import Session
+from typing import Dict
+import asyncio
+from api.Social.models.user import User
+from api.Social.models.notification import Notification
+
+
+class NotificationService():
+    
+    def __init__(self):
+        
+        
+        self.user_event_queues:Dict[str, asyncio.Queue] = {}
+        
+        
+    async def event_generator(self, user_id:str):
+        
+        if user_id not in self.user_event_queues:
+            self.user_event_queues[user_id] = asyncio.Queue()
+            
+        while True:
+            event = await self.user_event_queues[user_id].get()
+            yield f"data: {event}"
+            
+    
+        
