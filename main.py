7e1dffd14f571d9edd1d61ac5dde026a396b262b
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-05 23:39:53 +0100

    modified the imports for the base and engine

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI 
-from ApiApp import model
-from ApiApp.db import engine
-from ApiApp.routers import posts, users, order, auth
 from pydantic_settings import BaseSettings
 from ApiApp.config import settings
 from fastapi.middleware.cors import CORSMiddleware
+from api.Social.utils.database import Base, engine
 
 
 app = FastAPI()
@@ -16,10 +14,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
-app.include_router(posts.router)
-app.include_router(users.router)
-app.include_router(order.router)
-app.include_router(auth.router)
+
 model.Base.metadata.create_all(bind=engine)
 
 
